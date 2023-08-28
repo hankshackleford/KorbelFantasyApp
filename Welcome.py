64e0fa6bd56951a2e43372e8d31dfdb3862609af
hankshackleford <143135567+hankshackleford@users.noreply.github.com>
2023-08-28 12:34:46 -0400

    Add files via upload

--- Welcome.py
+++ Welcome.py
@@ -6,12 +6,12 @@ Created on Fri Aug 25 15:19:37 2023
 """
 
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+
 
 st.set_page_config(
     page_title="Korbel Fantasy League",
     page_icon="🏈",
+    layout="wide"
 )
 
 st.write("# Welcome to the Official App of the Korbel Fantasy League.")
@@ -19,8 +19,7 @@ st.text('Select a page from the sidebar to get started.')
 st.text('Player Data: analyze data on players for 2023 (including projections) and 2022 (including draft recap')
 st.text('Past Seasons: recap each season, including full season stats')
 st.text('Team Owners: assess total historic performance of all team owners')
-
-
+st.text('Comparison of Performance and Value: use this interactive chart to explore draft value and player performance')
 
 
 st.sidebar.success("Select a page above.")
