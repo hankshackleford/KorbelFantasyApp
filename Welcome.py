dbb66bbfdcbfd58e7813aa1750c6c2c11f9a79f6
hankshackleford <143135567+hankshackleford@users.noreply.github.com>
2023-08-30 22:05:06 -0400

    Add files via upload

--- Welcome.py
+++ Welcome.py
@@ -16,10 +16,16 @@ st.set_page_config(
 
 st.write("# Welcome to the Official App of the Korbel Fantasy League.")
 st.text('Select a page from the sidebar to get started.')
-st.text('Player Data: analyze data on players for 2023 (including projections) and 2022 (including draft recap')
+st.text('Player Data: analyze data on players for 2023 (including projections) and 2022 (including draft recap)')
 st.text('Past Seasons: recap each season, including full season stats')
 st.text('Team Owners: assess total historic performance of all team owners')
 st.text('Comparison of Performance and Value: use this interactive chart to explore draft value and player performance')
+
+st.subheader('Version History')
+st.text('V1.05, 8/30/2023: Updated 2023 player projections, added rookies from 2023 draft class')
+st.text('V1.00, 8/28/2023: App release')
+
+
 
 
 st.sidebar.success("Select a page above.")
